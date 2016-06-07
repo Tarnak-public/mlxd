1e260700ebe6b099298f1ce8de8ce013cbf14e80
ericksot <todd.erickson@wwt.com>
2016-06-06 22:18:27 -0500

    adding debugging of to equation

--- mlxd.c
+++ mlxd.c
@@ -639,8 +639,8 @@ calc_to(float ta,  int vcp)
         temperatures[i] = exp((log(   (v_ir_comp + pow((ta + 273.15), 4))   )/4.0))
                 - 273.15;
         temperaturesInt[i] = (unsigned short)((temperatures[i] + 273.15) * 10.0) ;
-        printf("TE Test Temperatures index: %d value: %f \n", i, temperatures[i]);
-        printf("TE Test TemperaturesInt index: %d value: %d \n", i, temperaturesInt[i]);
+        //printf("TE Test Temperatures index: %d value: %f \n", i, temperatures[i]);
+        //printf("TE Test TemperaturesInt index: %d value: %d \n", i, temperaturesInt[i]);
     }
 }
 
