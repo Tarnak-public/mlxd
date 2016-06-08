e614b50d130b5e741545db1819c7d6b5e1e65993
ericksot <todd.erickson@wwt.com>
2016-06-07 20:44:20 -0500

    adding debugging of to equation

--- mlxd.c
+++ mlxd.c
@@ -610,9 +610,9 @@ calc_to(float ta,  int vcp)
 */
     for (i = 0; i < 64; i++) {
         printf("EEPROM[i] %d: %d \n", i, EEPROM[i]);
-        a_ij[i] = ((float) a_common + EEPROM[i] * pow(2, a_i_scale))
+        a_ij[i] = (a_common + EEPROM[i] * pow(2, a_i_scale))
                 / pow(2, (3 - resolution));
-        a_ij[i] += i * 5;
+
         printf("a_ij %d: %f \n", i, a_ij[i]);
         printf("EEPROM[0x40 + i] %d: %d \n", i, EEPROM[0x40 + i]);
         b_ij[i] = EEPROM[0x40 + i];
@@ -620,7 +620,8 @@ calc_to(float ta,  int vcp)
             b_ij[i] -= 256;
         b_ij[i] = b_ij[i] / (pow(2, b_i_scale) * pow(2, (3 - resolution)));
         printf("b_ij %d: %f \n", i, b_ij[i]);
-        v_ir_off_comp = irData[i] - (a_ij[i] + b_ij[i] * (ta - 25.0));
+
+        v_ir_off_comp = (irData[i] * 10) - (a_ij[i] + b_ij[i] * (ta - 25.0));
         printf("v_ir_off_comp %d: %f \n", i, v_ir_off_comp);
         v_ir_tgc_comp = v_ir_off_comp - tgc * v_cp_off_comp;
         printf("v_ir_tgc_comp %d: %f \n", i, v_ir_tgc_comp);
