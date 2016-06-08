8c4a5e38a23045123cc45f83f3387f3a5559c5e7
ericksot <todd.erickson@wwt.com>
2016-06-07 21:14:59 -0500

    adding debugging of to equation

--- mlxd.c
+++ mlxd.c
@@ -643,12 +643,7 @@ calc_to(float ta,  int vcp)
         //printf("v_ir_norm %d: %f \n", i, v_ir_norm);
         v_ir_comp = v_ir_norm / emissivity;
         //printf("v_ir_comp %d: %f \n", i, v_ir_comp);
-        val0 = (ta + 273.15);
-        val0 = pow(val0, 4);
-        val0 = v_ir_comp + val0;
-        val0 = pow(val0, 1/4.0);
-        printf("test temp eq index: %d value: %f \n", i, pow((v_ir_comp + pow((ta + 273.15), 4)), 1/4.0) - 273.15);
-        temperatures[i] = val0 - 273.15;
+        temperatures[i] = pow((v_ir_comp + pow((ta + 273.15), 4)), 1/4.0) - 273.15;
         temperaturesInt[i] = (unsigned short)((temperatures[i] + 273.15) * 100.0);
         printf("TE Test Temperatures index: %d value: %f \n", i, temperatures[i]);
         printf("TE Test TemperaturesInt index: %d value: %d \n", i, temperaturesInt[i]);
